801e69faa2809a46f5d6aae959973b7dda4b256d
jabier espinal <jabierespinal@gmail.com>
2018-02-11 01:58:08 +0000

    angular 1

--- lvl1/app.js
+++ lvl1/app.js
@@ -1,1 +1,10 @@
 var app = angular.module('gemStore', []);
+
+(function(){
+    var app = angular.module('store', [ ]);
+    
+    app.controller('StoreController', function(){
+        this.product = gem;
+    });
+    . . .
+})();
