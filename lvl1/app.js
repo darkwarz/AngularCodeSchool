22da9b4a1cbb48d161e8835f8b9409b9641331dc
jabier espinal <jabierespinal@gmail.com>
2018-02-13 10:17:08 +0000

    learning more code

--- lvl1/app.js
+++ lvl1/app.js
@@ -1,10 +1,10 @@
 var app = angular.module('gemStore', []);
 
 (function(){
-    var app = angular.module('store', [ ]);
-    
+    var gem = { name: 'Azurite', price: 2.95 };
+    var app = angular.module('gemStore', [ ]);
     app.controller('StoreController', function(){
         this.product = gem;
     });
-    . . .
+    
 })();
